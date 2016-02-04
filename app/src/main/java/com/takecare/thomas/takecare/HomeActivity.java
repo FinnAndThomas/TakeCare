963bf260484d3ccc345bd26bf7295e967863f43f
Caron Thomas <thomas.caron@epitech.eu>
2016-02-04 16:01:09 -0600

    add fragments
    home fragment
    settings fragment

--- app/src/main/java/com/takecare/thomas/takecare/HomeActivity.java
+++ app/src/main/java/com/takecare/thomas/takecare/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.takecare.thomas.takecare;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,7 +19,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, HomeFragement.OnFragmentInteractionListener {
+
+    static  private  String ACTIVITYNAME = "HomeActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +31,11 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Log.d(ACTIVITYNAME, "new Alarm");
             }
         });
 
@@ -59,7 +66,27 @@ public class HomeActivity extends AppCompatActivity
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void replaceFragment(Fragment fragment) {
+          FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+          transaction.replace(R.id.container, fragment);
+          transaction.addToBackStack(null);
+          transaction.commit();
+    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -67,18 +94,21 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
+            replaceFragment(new HomeFragement());
+        } else if (id == R.id.nav_history) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
